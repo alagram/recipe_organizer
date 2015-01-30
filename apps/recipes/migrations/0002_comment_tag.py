6c1d802de5965f9504cb4df112a763f5785b2d85
Albert Agram <albert.agram@gmail.com>
2015-01-30 01:04:32 +0000

    list, create recipes. adde comments

---
+++ apps/recipes/migrations/0002_comment_tag.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recipes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('body', models.TextField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('recipe', models.ForeignKey(to='recipes.Recipe')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('name', models.CharField(max_length=50)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from rest_framework import generics
 from apps.recipes.serializers import *
 
-
-# Create your views here.
 class RecipeList(generics.ListAPIView):
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
 
+class RecipeDetail(generics.RetrieveAPIView):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
 
-class IngredientList(generics.ListAPIView):
-    serializer_class = IngredientSerializer
-    queryset = Ingredient.objects.all()
+class AddRecipe(generics.CreateAPIView):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+
+class AddComment(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
