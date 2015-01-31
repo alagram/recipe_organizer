7b2e050c52c701de81f2473d4e6f8cf27fb960f6
Albert Agram <albert.agram@gmail.com>
2015-01-31 01:07:41 +0000

    updated RecipeDetail to RetrieveUpdateDestroyAPIView

--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -6,7 +6,7 @@ class RecipeList(generics.ListAPIView):
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
 
-class RecipeDetail(generics.RetrieveAPIView):
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
 
