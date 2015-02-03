d3e86472be5c5c6a9feb39c840b4c5781046dc63
Albert Agram <albert.agram@gmail.com>
2015-02-03 17:51:03 +0000

    fix 500 error

--- apps/recipes/serializers.py
+++ apps/recipes/serializers.py
@@ -12,20 +12,21 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True)
     comments = serializers.SerializerMethodField()
     class Meta:
         model = Recipe
         depth = 1
 
     def create(self, validated_data):
-        ingredient_data = validated_data.pop("ingredients")
+        ingredient_data = validated_data.pop('ingredients')
         recipe = Recipe.objects.create(**validated_data)
 
         for ingredient in ingredient_data:
             try:
                 ingredient = Ingredient.objects.get(name=ingredient["name"])
-            except Ingredient.DoesNotExis:
-                ingredient = Ingredient.objects.create(**ingredient_data)
+            except Ingredient.DoesNotExist:
+                ingredient = Ingredient.objects.create(**ingredient)
             recipe.ingredients.add(ingredient)
 
         return recipe
